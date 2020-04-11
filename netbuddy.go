e13c4da54c37e0d3cfbf37cb49994de117815b5d
Jack Dockerty <jdockerty19@gmail.com>
2020-04-11 14:18:32 +0100

    Reformatting and added go.sum after Go version upgrade.

--- netbuddy.go
+++ netbuddy.go
@@ -2,30 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"github.com/apparentlymart/go-cidr/cidr"
-	// "github.com/voxelbrain/goptions"
-	"strings"
-	"log"
+	"net"
 	"flag"
+	"log"
 	"os"
-
+	"strings"
 )
 
-
-
 type SubnetInfo struct {
-	networkAddress net.IP
-	broadcastAddress net.IP
+	networkAddress      net.IP
+	broadcastAddress    net.IP
 	firstUsuableAddress net.IP
-	lastUsuableAddress net.IP
-	totalAddressCount uint64
+	lastUsuableAddress  net.IP
+	totalAddressCount   uint64
 }
 
 type PortsInfo struct {
-	commonPortNumbers []int
+	commonPortNumbers      []int
 	transportLayerProtocol string
-	extraInfoLink string
+	extraInfoLink          string
 }
 
 func parseIPInfo(ipString string) (net.IP, *net.IPNet) {
@@ -49,23 +45,23 @@ func subnetIterations(ipnet *net.IPNet, iterations int) {
 	_, lastAddress := getNetworkAndBroadcast(ipnet)
 	prefixBits, _ := ipnet.Mask.Size()
 
-	for i := 1; i < iterations + 1; i++ {
+	for i := 1; i < iterations+1; i++ {
 		nextNetworkAddress := cidr.Inc(lastAddress)
 		nextPrefixString := fmt.Sprint(nextNetworkAddress, "/", prefixBits)
-		fmt.Printf("[%d] Next subnet: %s\n",i, nextPrefixString)
+		fmt.Printf("[%d] Next subnet: %s\n", i, nextPrefixString)
 		_, nextIPNet := parseIPInfo(nextPrefixString)
 		_, lastAddress = getNetworkAndBroadcast(nextIPNet)
 
 		nextSubnetInfo := getSubnetInfo(nextIPNet)
 		printSubnetInfo(nextSubnetInfo)
-			
+
 	}
 
 }
 
 func printSubnetInfo(subnet SubnetInfo) {
-	fmt.Printf("Network: %s\nFirst assignable: %s\nLast assignable: %s\nBroadcast: %s\n", 
-	subnet.networkAddress, subnet.firstUsuableAddress, subnet.lastUsuableAddress, subnet.broadcastAddress)
+	fmt.Printf("Network: %s\nFirst assignable: %s\nLast assignable: %s\nBroadcast: %s\n",
+		subnet.networkAddress, subnet.firstUsuableAddress, subnet.lastUsuableAddress, subnet.broadcastAddress)
 }
 
 func getSubnetInfo(ipnet *net.IPNet) SubnetInfo {
@@ -75,21 +71,21 @@ func getSubnetInfo(ipnet *net.IPNet) SubnetInfo {
 	totalAddresses := getAddressCount(ipnet)
 
 	subnetInfoResponse := SubnetInfo{
-		networkAddress: networkAddr,
-		broadcastAddress: broadcastAddr,
+		networkAddress:      networkAddr,
+		broadcastAddress:    broadcastAddr,
 		firstUsuableAddress: firstUsuableAddr,
-		lastUsuableAddress: lastUsuableAddr,
-		totalAddressCount: totalAddresses,
+		lastUsuableAddress:  lastUsuableAddr,
+		totalAddressCount:   totalAddresses,
 	}
-	
+
 	return subnetInfoResponse
 }
 
 func populatePortsInfo(portNums []int, transportProto, link string) PortsInfo {
 	portsInfoResponse := PortsInfo{
-		commonPortNumbers : portNums,
-		transportLayerProtocol : transportProto,
-		extraInfoLink : link,
+		commonPortNumbers:      portNums,
+		transportLayerProtocol: transportProto,
+		extraInfoLink:          link,
 	}
 	return portsInfoResponse
 }
@@ -97,19 +93,19 @@ func populatePortsInfo(portNums []int, transportProto, link string) PortsInfo {
 func getWikiName(service string) string {
 
 	abbreviationsToWikiName := map[string]string{
-		"dns" : "Domain_Name_System",
-		"dhcp" : "Dynamic_Host_Configuration_Protocol",
-		"rdp" : "Remote_Desktop_Protocol",
-		"smtp" : "Simple_Network_Management_Protocol",
-		"ssh" : "Secure_Shell",
-		"telnet" : "telnet",
-		"ftp" : "File_Transfer_Protocol",
-		"http" : "Hypertext_Transfer_Protocol",
-		"https" : "HTTPS",
-		"imap" : "Internet_Message_Access_Protocol",
-		"pop3" : "Post_Office_Protocol",
-		"ldap" : "Lightweight_Directory_Access_Protocol",
-		"bgp" : "Border_Gateway_Protocol", 
+		"dns":    "Domain_Name_System",
+		"dhcp":   "Dynamic_Host_Configuration_Protocol",
+		"rdp":    "Remote_Desktop_Protocol",
+		"smtp":   "Simple_Network_Management_Protocol",
+		"ssh":    "Secure_Shell",
+		"telnet": "telnet",
+		"ftp":    "File_Transfer_Protocol",
+		"http":   "Hypertext_Transfer_Protocol",
+		"https":  "HTTPS",
+		"imap":   "Internet_Message_Access_Protocol",
+		"pop3":   "Post_Office_Protocol",
+		"ldap":   "Lightweight_Directory_Access_Protocol",
+		"bgp":    "Border_Gateway_Protocol",
 	}
 
 	return abbreviationsToWikiName[service]
@@ -117,7 +113,7 @@ func getWikiName(service string) string {
 
 func printPortInfo(portInfo PortsInfo) {
 	fmt.Printf("Port Numbers: %d\nTransport Protocol(s): %s\nFor more information on this protocol visit %s\n",
-	portInfo.commonPortNumbers, portInfo.transportLayerProtocol, portInfo.extraInfoLink)
+		portInfo.commonPortNumbers, portInfo.transportLayerProtocol, portInfo.extraInfoLink)
 }
 
 func getCommonPorts(service string) PortsInfo {
@@ -126,25 +122,25 @@ func getCommonPorts(service string) PortsInfo {
 
 	switch strings.ToLower(service) {
 	case "dns":
-		info := populatePortsInfo([]int{53}, "UDP", wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{53}, "UDP", wikiString+getWikiName(service))
 		return info
 
 	case "dhcp":
-		info := populatePortsInfo([]int{67,68}, "UDP", wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{67, 68}, "UDP", wikiString+getWikiName(service))
 		return info
 
 	case "rdp":
-		info := populatePortsInfo([]int{3389}, "TCP + UDP", wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{3389}, "TCP + UDP", wikiString+getWikiName(service))
 		return info
 
 	case "ldap":
-		info := populatePortsInfo([]int{389}, "TCP + UDP", wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{389}, "TCP + UDP", wikiString+getWikiName(service))
 		return info
 
 	case "bgp":
-		info := populatePortsInfo([]int{179}, "TCP", wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{179}, "TCP", wikiString+getWikiName(service))
 		return info
-	
+
 	// Most of the common ports can be retrieved via the in-built net package
 	default:
 		transportProtocol := "tcp"
@@ -154,7 +150,7 @@ func getCommonPorts(service string) PortsInfo {
 			log.Fatal(err)
 		}
 
-		info := populatePortsInfo([]int{portNum}, strings.ToUpper(transportProtocol), wikiString + getWikiName(service))
+		info := populatePortsInfo([]int{portNum}, strings.ToUpper(transportProtocol), wikiString+getWikiName(service))
 		return info
 
 	}
@@ -165,7 +161,20 @@ func ipv4PrivateAddressRange() {
 	fmt.Printf("\t10.0.0.0 - 10.255.255.255\n\t172.16.0.0 - 172.31.255.255\n\t192.168.0.0 - 192.168.255.255\n")
 }
 
-
+func subnetCmdHelp() {
+	fmt.Println("Usage: netbuddy subnet <arg> <input>")
+	fmt.Println("Args:\n\t-display: Shows various information about a particular IP and CIDR, e.g. 192.168.4.20/19")
+	fmt.Println("\t-count: Show the total number of addresses in the provided network.")
+	fmt.Println("\t-iterate: Show the next X iterations of a particular prefix to the network.")
+	fmt.Println("\nExamples: \n\t netbuddy subnet -count 172.31.5.9/19\n\t netbuddy subnet -iterate 2 192.168.0.0/24")
+}
+func showCmdHelp() {
+	fmt.Println("Usage: netbuddy show <option> <input>")
+	fmt.Println("Options:\n\tipv4range - Show RFC 1918 IPv4 address range. \tNote: This does not take an input.")
+	fmt.Println("\tmac - Show MAC address of an interface.")
+	fmt.Println("\tservice - Shows port and information for a particular service e.g. SSH")
+	fmt.Println("\nExamples: \n\t netbuddy show service ssh\n\t netbuddy show ipv4range")
+}
 
 func main() {
 	showCmd := flag.NewFlagSet("show", flag.ExitOnError)
@@ -186,24 +195,17 @@ func main() {
 		case "service":
 			portInfo := getCommonPorts(os.Args[3])
 			printPortInfo(portInfo)
+		case "help":
+			showCmdHelp()
 		default:
-			fmt.Println("Usage: netbuddy show <option> <input>")
-			fmt.Println("Options:\n\tipv4range - Show RFC 1918 IPv4 address range. \tNote: This does not take an input.")
-			fmt.Println("\tmac - Show MAC address of an interface.")
-			fmt.Println("\tservice - Shows port and information for a particular service e.g. SSH")
-			fmt.Println("\nExamples: \n\t netbuddy show service ssh\n\t netbuddy show ipv4range")
+			showCmdHelp()
 		}
-	
-	
+
 	case "subnet":
 		subnetCmd.Parse(os.Args[2:])
-		
+
 		if os.Args[2] == "help" {
-			fmt.Println("Usage: netbuddy subnet <arg> <input>")
-			fmt.Println("Args:\n\t-display: Shows various information about a particular IP and CIDR, e.g. 192.168.4.20/19")
-			fmt.Println("\t-count: Show the total number of addresses in the provided network.")
-			fmt.Println("\t-iterate: Show the next X iterations of a particular prefix to the network. E.g. Input: 192.168.0.0/24. Output: 192.168.1.0/24, 192.168.2.0/24")
-			fmt.Println("\nExamples: \n\t netbuddy subnet -count 172.31.5.9/19\n\t netbuddy subnet -iterate 2 192.168.0.0/24")
+			subnetCmdHelp()
 		}
 		if len(*subnetDisplay) != 0 {
 			_, ipnet := parseIPInfo(*subnetDisplay)
@@ -223,6 +225,5 @@ func main() {
 		}
 
 	}
-
 
 }
